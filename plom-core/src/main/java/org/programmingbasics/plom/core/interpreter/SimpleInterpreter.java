29cea3786edca494274c6a4ddb09e6eb5e78aaa1
Ming <github@programmingbasics.org>
2020-06-13 18:32:14 -0400

    Making an input: function too so that I can make some interactive tests.

--- plom-core/src/main/java/org/programmingbasics/plom/core/interpreter/SimpleInterpreter.java
+++ plom-core/src/main/java/org/programmingbasics/plom/core/interpreter/SimpleInterpreter.java
@@ -11,7 +11,9 @@ import org.programmingbasics.plom.core.interpreter.MachineContext.PrimitiveBlock
 
 import elemental.client.Browser;
 import elemental.dom.Document;
+import elemental.dom.Element;
 import elemental.html.DivElement;
+import elemental.html.InputElement;
 
 /**
  * In order to work out how the language will work, I need a simple 
@@ -70,9 +72,39 @@ public class SimpleInterpreter
           continueRun();
         });
         doc.getBody().appendChild(container);
+        container.querySelector("a").focus();
       }
     };
     scope.addVariable("print:", printFun);
+    
+    Value inputFun = new Value();
+    inputFun.type = Type.makePrimitiveBlockingFunctionType(Type.STRING, Type.STRING);
+    inputFun.val = new PrimitiveFunction.PrimitiveBlockingFunction() {
+      @Override
+      public void call(PrimitiveBlockingFunctionReturn blockWait, List<Value> args)
+      {
+        Document doc = Browser.getDocument();
+        DivElement container = doc.createDivElement();
+        container.setAttribute("style", "position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); background-color: white; padding: 0.5em; border: 1px solid black; text-align: center;");
+        container.setInnerHTML("<div></div><div><form><input type=\"text\"></form></div><div><a href=\"#\">Ok</a></div>");
+        container.querySelectorAll("div").item(0).setTextContent(args.get(0).getStringValue());
+        container.querySelector("form").setOnsubmit((e) -> {
+          e.preventDefault();
+          doc.getBody().removeChild(container);
+          blockWait.unblockAndReturn(Value.createStringValue(((InputElement)container.querySelector("input")).getValue()));
+          continueRun();
+        });
+        container.querySelector("a").setOnclick((e) -> {
+          e.preventDefault();
+          doc.getBody().removeChild(container);
+          blockWait.unblockAndReturn(Value.createStringValue(((InputElement)container.querySelector("input")).getValue()));
+          continueRun();
+        });
+        doc.getBody().appendChild(container);
+        container.querySelector("input").focus();
+      }
+    };
+    scope.addVariable("input:", inputFun);
   }
   
   public void runCode(MachineContext ctx) throws ParseException, RunException
