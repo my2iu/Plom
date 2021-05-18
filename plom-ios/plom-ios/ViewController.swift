7450cafd0f87816739617718ea7a776cb3be2a4e
Ming <git@wobastic.com>
2021-05-18 17:07:49 -0400

    Making an empty ios xcode project

---
+++ plom-ios/plom-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plom-ios
+//
+//  Created by World on 2021-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
