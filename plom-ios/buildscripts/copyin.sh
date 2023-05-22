d57ca6f55ed426be4831543ae18f6571dfb4c549
Ming <git@user00.com>
2023-05-22 01:00:44 -0400

    For ios, using the simple template index.html from the web version, not the android templates

--- plom-ios/buildscripts/copyin.sh
+++ plom-ios/buildscripts/copyin.sh
@@ -5,6 +5,7 @@ ${MVN} install -DskipTests
 mkdir -p ../plom-ios/html/plom
 cp -r target/plom-core-0.1-SNAPSHOT/ ../plom-ios/html/plom
 cp -r ../plom-android/app/src/main/assets/templates ../plom-ios/html
+cp src/main/webapp/plomweb.html ../plom-ios/html/templates/simple/web/index.html
 cd ../plom-ios/html/plom
 mv plomcore/*.cache.js plomcore/plomdirect.js
 sed -i '' -e "s/plomcore.nocache.js/plomdirect.js/g" index.html
