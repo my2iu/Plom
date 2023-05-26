96eb41a3d93dfb70ad357246281caea76a465560
Ming <git@user00.com>
2023-05-26 02:36:59 -0400

    Making a stub for eventually supporting editing of extra files in the app

---
+++ plom-core/src/main/java/org/programmingbasics/plom/core/FileViewerPanel.java
@@ -0,0 +1,39 @@
+package org.programmingbasics.plom.core;
+
+import org.programmingbasics.plom.core.GlobalsPanel.LoadClassViewCallback;
+import org.programmingbasics.plom.core.GlobalsPanel.LoadFunctionCodeViewCallback;
+import org.programmingbasics.plom.core.GlobalsPanel.LoadFunctionSigViewCallback;
+import org.programmingbasics.plom.core.view.SvgCodeRenderer;
+
+import elemental.client.Browser;
+import elemental.dom.Document;
+import elemental.html.DivElement;
+import elemental.svg.SVGDocument;
+
+public class FileViewerPanel implements AutoCloseable
+{
+  Document doc = Browser.getDocument();
+  ModuleCodeRepository repository;
+  DivElement mainDiv;
+
+  public FileViewerPanel(DivElement mainDiv, ModuleCodeRepository repository)
+  {
+    this.mainDiv = mainDiv;
+    this.repository = repository;
+//    this.viewSwitchCallback = callback;
+//    this.functionSigCallback = functionSigCallback;
+//    this.classViewCallback = classViewCallback;
+//    widthCalculator = new SvgCodeRenderer.SvgTextWidthCalculator((SVGDocument)Browser.getDocument());
+    rebuildView();
+  }
+  
+  void rebuildView()
+  {
+    Document doc = Browser.getDocument();
+    mainDiv.setInnerHTML(UIResources.INSTANCE.getGlobalsPanelHtml().getText());
+  }
+  
+  @Override public void close()
+  {
+  }
+}
