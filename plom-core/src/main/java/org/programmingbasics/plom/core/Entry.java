443b701ff39a377568cde2de1d9c916fe0dcc6d1
Ming <git@user00.com>
2023-06-09 17:02:57 -0400

    Notes from tojam2023 about fixes needed

--- plom-core/src/main/java/org/programmingbasics/plom/core/Entry.java
+++ plom-core/src/main/java/org/programmingbasics/plom/core/Entry.java
@@ -67,6 +67,57 @@ TODO:
 - when editing methods, not handling blur of type fields properly
 - when editing methods, type fields should use new svg rendering
 - when editing methods, have a quick erase button for auto-resizing text input?
+
+
+TODO list from TOJam 2023
+- [ ] New project in external folder doesn’t copy external files or other files on iOS
+- [ ] iOS showing stuff outside web/ folder in extra files
+- [ ] Open project directories straight from web version
+- [ ] Send logging messages and crash messages back to show in console or elsewhere
+- [ ] Clear console messages
+- [ ] On iOS, you possibly have to double tap to get copy and erase buttons of selections to register
+- [ ] Should throw an error if you don’t chain your constructors—especially to @object.new
+- [ ] Wkwebview not being cleaned when exiting (after exiting and reentering to save a few times, there will be a lot of WKWebViews hanging around that the Safari debugger can find)
+- [ ] Run error shows the line but not the file
+- [ ] Escape the HTML put in strings
+- [ ] Warning when nothing returned from method when something expected
+- [ ] Auto fill of lambda function args strips out the leading . from arg name
+- [ ] If there is a problem with a method signature (e.g. unknown return type because of unknown class name) then flag it in UI
+- [ ] If there is a problem with a variable declaration (e.g. unknown type due to unknown class name) then flag it in UI
+- [ ] If there is a parse error in ParseToAst, especially deep in a lambda, there is no way to report where in the code the error was found and then to propagate that onwards
+- [ ] Don’t need people to click on OK after changing method signatures
+- [ ] RepositoryScope unboundtypetotype should log errors when constructing types
+- [ ] Are event handlers (jsobject from function) logging errors?
+- [ ] Are methods listed alphabetically in suggestions?
+- [ ] Android version should filter out .git folders in extra files (or any dir starting with .)?
+- [ ] Method return type and extends should use proper code widget
+- [ ] When hooking in project with an existing dir in Android (possibly empty project), it copies in the wrong stuff
+- [ ] No warning when using member variables before calling super constructor in constructor
+- [ ] Comment out code
+- [ ] Code suggestions should be case insensitive
+- [ ] Generic  JS object wrap and unwrap Plom objects
+- [ ] Super doesn’t work for calling non-static methods
+- [ ] No suggestions for instance methods after calling a constructor
+- [ ] Better handling of big tablets where it’s a pain to reach around the screen to press buttons
+- [ ] In method signature editor, when editing a name part, pressing enter should move to the argument part
+- [ ] Line after an if with lambdas might have line height too short maybe if I can find an example again
+- [ ] Lambdas with a long function signature part sometimes has wrapping problems where the return type of the function signature wraps to an incorrect location
+- [ ] Allow extra blank lines in saved Plom files in case of hand editing
+- [ ] List of method suggestions doesn’t list everything available in a class if there are a lot of methods available
+- [ ] When typing, suggestion list should scroll back to top if you’ve scrolled it a bit before looking for something
+- [ ] Undo support
+- [ ] Specify UTF8 in index.html to ensure that .js files also default to UTF8?
+- [ ] Possible problem with resources loading in out of order in compiled .js (if something is delayed, then it doesn’t load correctly?)
+- [ ] Slow performance on slow Android phones maybe with larger projects?
+- [ ] Need break and continue commands
+- [ ] Iterator for @array
+- [ ] Need an iOS icon, maybe a phone with code on it (curly braces? Or 5 lines with some indented)
+- [ ] Add meta viewport tag to default index.html
+- [ ] Empty return in @void method not allowed
+- [ ] Increase size of minimize web output button so that you don’t accidentally keep hitting the hamburger menu instead
+- [ ] A separate @JSObject.from function: and @JSObject.from top level event handler: so that debugging hooks can be inserted properly
+- [ ] Back button in upper-left for android
+
  */
 
 public class Entry implements EntryPoint
