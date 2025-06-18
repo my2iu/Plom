e4c726e57173e8a1171be3ec739ca252f1fbe951
Ming <git@programmingbasics.org>
2025-06-18 16:53:48 -0400

    Making a Plom with the latest stdlib changes

--- plom-core/src/main/java/org/programmingbasics/plom/core/Entry.java
+++ plom-core/src/main/java/org/programmingbasics/plom/core/Entry.java
@@ -81,6 +81,9 @@ TODO:
 - Android run web window doesn't scroll?
 - Android keyboard doesn't have numbers in number mode. Does number input now have a period available? Maybe have a custom number keyboard?
 
+- make a primitive for @JS object.get:put:
+- @JS object.get: wraps a null or undefined as a @JS object. Should it return a Plom null instead?
+
 TODO list from TOJam 2023
 - [?] iOS New project in external folder doesn’t copy external files or other files
 - [X] iOS showing stuff outside web/ folder in extra files
